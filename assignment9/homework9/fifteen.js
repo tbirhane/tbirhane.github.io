7e9ac63196d1e0944a744ff2111d5bc5a4b5d417
tbirhane <tbirhane@mum.edu.com>
2019-06-14 17:14:49 -0500

    fifteen puzzle modified

--- assignment9/homework9/fifteen.js
+++ assignment9/homework9/fifteen.js
@@ -1,176 +1,180 @@
-var puzzle = [];
-var bpiece = [];
-
-
-window.onload = function() {
-    puzzle =  $("#puzzlearea div");
-    var row = 0, right = 0, top = 0;
-    //alert(puzzle.length);
-    //$('#puzzlearea').css('display','flex').css('flex-direction','row').css('flex-wrap', 'wrap');
-
-    for (var i=0;i<puzzle.length;i++){
-        $(puzzle[i]).addClass("puzzlepiece");
-        $(puzzle[i]).css('float','left');//.style.float = "left";
-        $(puzzle[i]).css('background-size','400px 400px')//.style.backgroundSize = "400px 400px";
-
-         bpiece[i] = [];
-         bpiece[i][0] = right;
-         bpiece[i][1] = top;
-        //
-
-        puzzle[i].style.backgroundPosition = "-"+bpiece[i][0]+"px -"+bpiece[i][1]+"px";
-        row ++;
-        if (row === 4){top += 100; right = 0; row = 0; } else {right +=100;}
+var puzzeModule = (function ()  {
+    var nRow = 0;
+    var nPieces = 0;
+    const COLS = 4;
+    const MAX = 9999;
+    const TIME_OUT = 1500;
+    var autoPlayStatus = false;
+    const MOVE = [{i: 0, j: 1}, {i: -1, j: 0}, {i: 0, j: -1}, {i: 1, j: 0}];
+    var emptySquarePosition = 15;
+    const NUM_STEPS = 50;
+    const TILE_SIZE = 100;
+    let arrMoveStatus = [];
+    let message = "Congradulation!!! You win the game";
+    /////////////////////////////////////////////////////////////
+    $(document).ready(function () {
+        jInit();
+    });
+    /////////////////////////////////////////////////////////////
+    function jInit () {
+        $("#puzzlearea div").each(function (index) {
+            initPuzze(this, index);
+            nPieces++;
+        });
+        $("#shufflebutton").click(shuffle);
     }
-
-   // var freemove = document.createElement("div");
-  //  $("puzzlearea").appendChild(freemove); //add a div that acts as the free move
-    var freemove = $("<div>");
-    $("#puzzlearea").append(freemove);
-    blankP(freemove);
-
-    puzzle = $("#puzzlearea div"); // "reassign" the array puzzle with the new div added
-    $("shufflebutton").click(shufflePuzzle);
-    movepiece();
-};
-
-// the function blankP is used to create the blank background for the space that represents the available move
-var blankP = function(bSpace){
-    bSpace.removeClass("movablepiece");
-    bSpace.addClass("puzzlepiece");
-    bSpace.css('float','left'); //style.float = "left";
-    bSpace.css('background-image','none');//style.backgroundImage = "none";
-    bSpace.css('border','2px solid white');//style.border = "2px solid white";
-};
-
-//the background_Position function is used to place the correct background piece to the number on the puzzlepiece.
-var background_Position = function(piece , item){
-    piece.style.backgroundPosition = "-"+bpiece[item-1][0]+"px -"+bpiece[item-1][1]+"px";
-};
-
-// the regularP function is used to apply the background to a numbered piece.
-var regularP = function(p){
-    p.addClassName("puzzlepiece");
-    p.style.border = "2px solid black";
-    p.style.backgroundImage = "url(background.jpg)";
-    p.style.backgroundSize = "400px 400px";
-};
-
-//the shuffluePuzzle function is used to shullfe each puzzle on the page.
-function shufflePuzzle(){
-    var numArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-    for (var i=puzzle.length; i>0; i){
-        var j = Math.floor(Math.random() * i);
-        var x = numArray[--i];
-        var test = numArray[j];
-        if(test == "0") {
-            puzzle[i].addClassName("puzzlepiece");
-            blankP(puzzle[i]);
-            puzzle[i].innerHTML = "";
-        }
-        else{
-            puzzle[i].innerHTML = numArray[j];
-            regularP(puzzle[i]);
-            background_Position(puzzle[i], test);
-        }
-        numArray[j] = x;
+ function getBoxIndex (piece) {
+        let left = $(piece).css('left');
+        let top = $(piece).css('top');
+        let x = parseInt(left);
+        let y = parseInt(top);
+        let col = Math.floor(x / TILE_SIZE);
+        let row = Math.floor(y / TILE_SIZE);
+        return row * COLS + col;
     }
-    mopiece();
-}
+    /////////////////////////////////////////////////////////////
+function getBoxPosition (i) {
+        return {x: ((i % COLS) * TILE_SIZE), y: (Math.floor(i / COLS) * TILE_SIZE)};
+    };
+    /////////////////////////////////////////////////////////////
+ function move (piece, newPosition) {
+        let currentPosition = getBoxIndex(piece);
+        if (checkMove(currentPosition, emptySquarePosition) === false) return false;
+        //Tracking
+        if (autoPlayStatus === false) {
+            let moveStatus = new MoveStatus(piece, currentPosition, newPosition);
+            arrMoveStatus.push(moveStatus);
+        }
+        emptySquarePosition = currentPosition;
+        console.log(emptySquarePosition);
+        setPosition(piece, newPosition);
+    };
+    /////////////////////////////////////////////////////////////
+ function showMessage (msg) {
+        $("#status").text(msg).css('color', 'red');
+    };
 
-//this function places the class movablepiece
-var movePA = function(piece){
-    puzzle[piece].addClassName("movablepiece");
-};
+    /////////////////////////////////////////////////////////////
+    function cellIndex (position) {
+        let c = {};
+        c.i = position % COLS;
+        c.j = Math.floor(position / COLS);
+        return c;
+    };
+    /////////////////////////////////////////////////////////////
+   function checkMove (currentPosition, newPosition) {
+        let c1 = cellIndex(currentPosition);
+        let c2 = cellIndex(newPosition);
+        let d = Math.abs(c1.i - c2.i) + Math.abs(c1.j - c2.j);
+        return d === 1;
+    };
+    /////////////////////////////////////////////////////////////
+ function setPosition (piece, i) {
+        let box = getBoxPosition(i);
+        let left = box.x + 'px';
+        let top = box.y + 'px';
+        $(piece).css({'left': left, 'top': top});
+    };
+    /////////////////////////////////////////////////////////////
+    function initPuzze (piece, i) {
+        let box = getBoxPosition(i);
+        let position = -box.x + 'px ' + (-box.y) + 'px';
+        //boxesPosition.push(box);
+        $(piece).addClass("puzzlepiece");
+        setPosition(piece, i);
+        $(piece).css({'backgroundImage':'../background.jpg', 'backgroundPosition': position});
+        //add event handler
+        $(piece).click(tileClick);
 
+    };
+    /////////////////////////////////////////////////////////////
+ function tileClick () {
+        move (this, emptySquarePosition);
+        if (checkWin() === true)
+            showMessage(message);
+    };
+    /////////////////////////////////////////////////////////////
+   function shuffle () {
+        reset();
+        for (let i = 0; i < NUM_STEPS; i++)
+            randomMoveClick();
+        console.log(arrMoveStatus);
 
-//the movepiece function is used to actually move the piece that is clicked on into the space.
-var movepiece = function(){
-    var move = this.innerHTML;
-    var yon = this.hasOwnProperty('movablepiece');
-    var blank = 0;
-    if (yon){
-        for (var i=0;i<puzzle.length;i++){
-            blank = puzzle[i].innerHTML;
-            if (puzzle[i].innerHTML == ""){
-                puzzle[i].innerHTML = move;
-                this.innerHTML = blank;
+    };
+    /////////////////////////////////////////////////////////////
+    function reset() {
+        emptySquarePosition = 15;
+        arrMoveStatus = [];
+        showMessage("");
+        let arr = [];
+        for (let i = 0; i < emptySquarePosition; i++)
+            arr[i] = i;
+        drawPuzzle(arr);
+    };
+    /////////////////////////////////////////////////////////////
+    function drawPuzzle (arr) {
+        $("#puzzlearea div").each(function (index) {
+            setPosition(this, arr[index]);
+        });
+        console.log(arr);
+    };
 
-                regularP(puzzle[i]);
-                blankP(this);
+   function randomMove(max) {
+        let my_num = Math.floor(Math.random() * max);
+        return my_num % 4;
 
-                mopiece();
-                background_Position(puzzle[i], move);
+    };
+    /////////////////////////////////////////////////////////////
+    function randomMoveClick () {
+        let canMove = false;
+        showMessage("");
+        let nRows = Math.floor(nPieces / COLS) + 1;
+        let emmtyCell = cellIndex(emptySquarePosition);
+        let cellWillMove = {};
+        let my_num = null;
+        do {
+            my_num = randomMove(MAX);
+            cellWillMove.i = emmtyCell.i + MOVE[my_num].i;
+            cellWillMove.j = emmtyCell.j + MOVE[my_num].j;
+            if (cellWillMove.i >= 0 && cellWillMove.i < COLS) {
+                if (cellWillMove.j >= 0 && cellWillMove.j < nRows) {
+                    canMove = true;
+                }
             }
-        }
-    }
-};
-
-
-//the function mopiece is used to calculate which pieces are beside the space and are able to move, thus applying the 'movablepiece' class
-var mopiece = function(){
-    for (var i=0;i<puzzle.length;i++){
-        puzzle[i].removeClassName("movablepiece");  }
-    for (var i=0; i<puzzle.length; i++){
-        if (puzzle[i].innerHTML == ""){
-            puzzle[i].removeClassName("movablepiece");
-
-            switch(i){
-                case 0:
-                    movePA(i+1);
-                    movePA(i+4);
-                    break;
-                case 1:
-                case 2:
-                    movePA(i-1);
-                    movePA(i+1);
-                    movePA(i+4);
-                    break;
-                case 3:
-                    movePA(i-1);
-                    movePA(i+4);
-                    break;
-                case 4:
-                    movePA(i-4);
-                    movePA(i+4);
-                    movePA(i+1);
-                    break;
-                case 5:
-                case 6:
-                case 9:
-                case 10:
-                    movePA(i-4);
-                    movePA(i+4);
-                    movePA(i+1);
-                    movePA(i-1);
-                    break;
-                case 7:
-                case 11:
-                    movePA(i-4);
-                    movePA(i+4);
-                    movePA(i-1);
-                    break;
-                case 8:
-                    movePA(i-4);
-                    movePA(i+1);
-                    movePA(i+4);
-                    break;
-                case 12:
-                    movePA(i-4);
-                    movePA(i+1);
-                    break;
-                case 13:
-                case 14:
-                    movePA(i-4);
-                    movePA(i-1);
-                    movePA(i+1);
-                    break;
-                case 15:
-                    movePA(i-4);
-                    movePA(i-1);
-                    break;
+        } while (canMove === false);
+        let position = cellWillMove.j * COLS + cellWillMove.i;
+        let piece = getPieceByPosition(position);
+        move(piece, emptySquarePosition);
+    };
+    /////////////////////////////////////////////////////////////
+   function getPieceByPosition (position) {
+        $("#puzzlearea div").each(function (index, value) {
+            if (position === getBoxIndex(this)) {
+                console.log("index=" + index);
+                move(value, emptySquarePosition);
+                console.log("new=" + emptySquarePosition);
+                return this;
             }
-        }
-        puzzle[i].observe('click', movepiece); }
-}   ;
 
+        });
+    };
+    /////////////////////////////////////////////////////////////
+       function MoveStatus (piece, oldPosition, newPosition) {
+        this.piece = piece;
+        this.oldPosition = oldPosition;
+        this.newPosition = newPosition;
+    };
+    /////////////////////////////////////////////////////////////
+ function checkWin () {
+        let res = true;
+        $("#puzzlearea div").each(function (index, value) {
+            console.log("index = " + index);
+            console.log("box index= " + getBoxIndex(this));
+            if (index !== getBoxIndex(this)) {
+                console.log("false");
+                res = false;
+            }
+        });
+        return res;
+    };
+})();
